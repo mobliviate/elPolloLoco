2b22299da273cb60822cca2f8ff55077941b364d
Marc Odermatt <info@marcodermatt.ch>
2025-06-05 21:02:47 +0200

    display math.round for chickens image 1.110

--- classes/chicken.class.js
+++ classes/chicken.class.js
@@ -3,5 +3,8 @@ class Chicken extends MovableObject {
 
     constructor() {
         super().loadImg('./img/3_enemies_chicken/chicken_normal/1_walk/1_w.png');
+
+        this.x = 200 + Math.random() * 500;
+        
     }
 }   
