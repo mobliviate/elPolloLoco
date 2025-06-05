613d10a82a35bbae60fcb54f1a9f4221aaa18b9b
Marc Odermatt <info@marcodermatt.ch>
2025-06-05 20:56:54 +0200

    display chicken image 1.10

--- classes/world.class.js
+++ classes/world.class.js
@@ -1,7 +1,7 @@
 class World {
     character = new Character();
     enemies = [new Chicken(), new Chicken(), new Chicken()];
-
+    canvas;
 
 
     ctx;
@@ -9,12 +9,22 @@ class World {
 
 
     constructor(canvas) {
+        this.canvas = canvas;
         this.ctx = canvas.getContext('2d');
         this.draw();
     }
 
     draw() {
+        this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
         this.ctx.drawImage(this.character.img, this.character.x, this.character.y, this.character.width, this.character.height);
 
+        this.enemies.forEach(enemy => {
+            this.ctx.drawImage(enemy.img, enemy.x, enemy.y, enemy.width, enemy.height);
+        });
+
+        let self = this;
+        requestAnimationFrame(function () {
+            self.draw();
+        });
     }
 }
