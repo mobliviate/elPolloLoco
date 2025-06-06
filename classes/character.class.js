ed5bc7999eb13ffb2cde3546891d8bfc44541c87
Marc Odermatt <info@marcodermatt.ch>
2025-06-06 23:15:35 +0200

    add endboss 2.15

--- classes/character.class.js
+++ classes/character.class.js
@@ -46,10 +46,7 @@ class Character extends MovableObject {
 
         if (this.world.keyboard.RIGHT || this.world.keyboard.LEFT) {
            
-            let i = this.currentImage % this.IMAGES_WALKING.length;
-            let path = this.IMAGES_WALKING[i];
-            this.img = this.imageCache[path];
-            this.currentImage++;
+            this.playAnimation(this.IMAGES_WALKING);
         }
     }, 100);
 
--- classes/movable-object.class.js
+++ classes/movable-object.class.js
@@ -24,8 +24,15 @@ class MovableObject {
         });
     }
 
+    playAnimation(images) {
+        let i = this.currentImage % this.IMAGES_WALKING.length;
+        let path = images[i];
+        this.img = this.imageCache[path];
+        this.currentImage++;
+    }
+
     moveRight() {
-        console.log("moveRight");
+
     }
 
     moveLeft() {
