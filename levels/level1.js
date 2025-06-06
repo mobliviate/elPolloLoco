995d943ccfed8e4a1c38e223b0627da60876d9bb
Marc Odermatt <info@marcodermatt.ch>
2025-06-06 22:43:51 +0200

    add level class 2.13

---
+++ levels/level1.js
@@ -0,0 +1,58 @@
+const level1 = new Level(
+    [
+        new Chicken(),
+        new Chicken(),
+        new Chicken(),
+    ],
+    [
+        new Cloud(),
+    ],
+    [
+        new BackgroundObject('./img/5_background/layers/air.png', -2876, 0),
+        new BackgroundObject('./img/5_background/layers/3_third_layer/1.png', -2876, 0),
+        new BackgroundObject('./img/5_background/layers/2_second_layer/1.png', -2876, 0),
+        new BackgroundObject('./img/5_background/layers/1_first_layer/1.png', -2876, 0),
+
+        new BackgroundObject('./img/5_background/layers/air.png', -2157, 0),
+        new BackgroundObject('./img/5_background/layers/3_third_layer/2.png', -2157, 0),
+        new BackgroundObject('./img/5_background/layers/2_second_layer/2.png', -2157, 0),
+        new BackgroundObject('./img/5_background/layers/1_first_layer/2.png', -2157, 0),
+
+        new BackgroundObject('./img/5_background/layers/air.png', -1438, 0),
+        new BackgroundObject('./img/5_background/layers/3_third_layer/1.png', -1438, 0),
+        new BackgroundObject('./img/5_background/layers/2_second_layer/1.png', -1438, 0),
+        new BackgroundObject('./img/5_background/layers/1_first_layer/1.png', -1438, 0),
+
+
+        new BackgroundObject('./img/5_background/layers/air.png', -719, 0),
+        new BackgroundObject('./img/5_background/layers/3_third_layer/2.png', -719, 0),
+        new BackgroundObject('./img/5_background/layers/2_second_layer/2.png', -719, 0),
+        new BackgroundObject('./img/5_background/layers/1_first_layer/2.png', -719, 0),
+
+        new BackgroundObject('./img/5_background/layers/air.png', 0, 0),
+        new BackgroundObject('./img/5_background/layers/3_third_layer/1.png', 0, 0),
+        new BackgroundObject('./img/5_background/layers/2_second_layer/1.png', 0, 0),
+        new BackgroundObject('./img/5_background/layers/1_first_layer/1.png', 0, 0),
+
+        new BackgroundObject('./img/5_background/layers/air.png', 719, 0),
+        new BackgroundObject('./img/5_background/layers/3_third_layer/2.png', 719, 0),
+        new BackgroundObject('./img/5_background/layers/2_second_layer/2.png', 719, 0),
+        new BackgroundObject('./img/5_background/layers/1_first_layer/2.png', 719, 0),
+
+        new BackgroundObject('./img/5_background/layers/air.png', 1438, 0),
+        new BackgroundObject('./img/5_background/layers/3_third_layer/1.png', 1438, 0),
+        new BackgroundObject('./img/5_background/layers/2_second_layer/1.png', 1438, 0),
+        new BackgroundObject('./img/5_background/layers/1_first_layer/1.png', 1438, 0),
+
+        new BackgroundObject('./img/5_background/layers/air.png', 2157, 0),
+        new BackgroundObject('./img/5_background/layers/3_third_layer/2.png', 2157, 0),
+        new BackgroundObject('./img/5_background/layers/2_second_layer/2.png', 2157, 0),
+        new BackgroundObject('./img/5_background/layers/1_first_layer/2.png', 2157, 0),
+
+        new BackgroundObject('./img/5_background/layers/air.png', 2876, 0),
+        new BackgroundObject('./img/5_background/layers/3_third_layer/1.png', 2876, 0),
+        new BackgroundObject('./img/5_background/layers/2_second_layer/1.png', 2876, 0),
+        new BackgroundObject('./img/5_background/layers/1_first_layer/1.png', 2876, 0),
+    ],  
+);
+
