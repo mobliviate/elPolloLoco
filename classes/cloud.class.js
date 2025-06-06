37de2520f60e57fa4f2248d07f2e31f05df7352c
Marc Odermatt <info@marcodermatt.ch>
2025-06-06 19:58:23 +0200

    animate clouds 2.01

--- classes/cloud.class.js
+++ classes/cloud.class.js
@@ -1,10 +1,18 @@
 class Cloud extends MovableObject {
     height = 400;
     width = 720;
+    y = 0;
     constructor() {
         super().loadImg('./img/5_background/layers/4_clouds/1.png');
 
-        this.x = 200 + Math.random() * 500;
-        this.y = 0;
+        this.x = 680;
+
+        this.animate();
+    }
+
+    animate() {
+        setInterval(() => {
+            this.x -= 0.25;
+        }, 1000 / 60);
     }
 }
